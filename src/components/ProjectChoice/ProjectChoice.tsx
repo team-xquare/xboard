7e5a3240ec95f9a9ecea774739b86cd76b72333d
oajh04 <cloudtubekr@naver.com>
2021-06-05 23:40:10 +0900

    ⚡️ :: [ProjectChoice] ProjectChoice

---
+++ src/components/ProjectChoice/ProjectChoice.tsx
@@ -0,0 +1,18 @@
+import ProjectHeader from './ProjectHeader/ProjectHeader';
+import ProjectBody from './ProjectBody/ProjectBody'
+import * as S from './styles'
+
+const ProjectChoice = () => {
+
+    return (
+        <>
+            <S.Wrapper>
+                <ProjectHeader />
+                <ProjectBody />
+            </S.Wrapper>
+            
+        </>
+    )
+}
+
+export default ProjectChoice;
