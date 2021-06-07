3f7d0c8fe35462c3804a303ec5fc1f86c7e7c348
oajh04 <cloudtubekr@naver.com>
2021-06-07 22:36:04 +0900

    ⚡️ :: [components] ProjectCard

--- src/components/ProjectChoice/ProjectBody/ProjectCard/ProjectCard.tsx
+++ src/components/ProjectChoice/ProjectBody/ProjectCard/ProjectCard.tsx
@@ -2,22 +2,22 @@ import React from 'react';
 import * as S from './styles'
 import { AiOutlineClockCircle } from 'react-icons/ai'
 
-const ProjectCard = () => {
+const ProjectCard = ({title, body, updated_at}) => {
 
     return (
         <>
             <S.CardWarpper>
                 <S.CardLeft>
-                    <S.CardTitle>Test</S.CardTitle>
+                    <S.CardTitle>{title}</S.CardTitle>
                     <S.CardDay>
                         <a><AiOutlineClockCircle size="20"/></a>
-                        Updated 7 hours ago
+                        {updated_at}
                     </S.CardDay>
                 </S.CardLeft>
 
                 <S.CardRigth>
                     <S.CardDescription>
-                        울려댔어 사이렌 텅빈 길거리엔 도망치다 흘린 칼자루와 피가 흥건해 우리 그때 어릴땐 뭐 몰랐지만 그냥 힘쎈 형이 젤로 멋잇는 형이었지 그땐
+                        {body}
                     </S.CardDescription>
                 </S.CardRigth>
                 
