c9f38226d82cb7d2d597d57e6ef9f8e9eaaaa06e
oajh04 <cloudtubekr@naver.com>
2021-06-07 22:36:34 +0900

    ⚡️ :: [components] projectbody

--- src/components/ProjectChoice/ProjectBody/ProjectBody.tsx
+++ src/components/ProjectChoice/ProjectBody/ProjectBody.tsx
@@ -1,16 +1,35 @@
-import React from 'react';
+import { useEffect, useState } from 'react';
+import { useRecoilState } from 'recoil';
+import organization from 'src/libs/api/organization';
+import { repoIdState } from 'src/libs/atom/RepoState/RepoState';
 import ProjectCard from './ProjectCard/ProjectCard';
 import * as S from './styles';
 
 const ProjectBody = () => {
+    const [ data, setData ] = useState<any>([])
+    const repoId = useRecoilState<any>(repoIdState);
+
+    useEffect(() => {
+        organization.getReposProject(repoId[0])
+        .then((res) => {
+            if(res.data.length !== 0){
+                console.log(res.data, "repo")
+                setData(res.data)
+            }
+        })
+    }, [repoId[0]])
+    
 
     return (
         <>
             <S.ProjectBodyWarpper>
-                <ProjectCard />
-                <ProjectCard />
-                <ProjectCard />
-                <ProjectCard />
+                {
+                    data.map((data) => {
+                        return(
+                            <ProjectCard key={data.id} title={data.name} body={data.body} updated_at={data.updated_at}/>
+                        )
+                    })
+                }
             </S.ProjectBodyWarpper>
         </>
     )
