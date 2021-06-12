4267b1a483941cffd6d168c2201c155fad504c4d
oajh04 <cloudtubekr@naver.com>
2021-06-12 22:36:34 +0900

    ⚡️ :: index 추가

---
+++ src/components/index.ts
@@ -0,0 +1,3 @@
+export { default as SideBar } from './SideBar/SideBar';
+export { default as ProjectChoice } from './ProjectChoice/ProjectChoice';
+export { default as Project } from './Project/Project';
