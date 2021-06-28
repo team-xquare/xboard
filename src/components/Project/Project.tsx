ec3b3563978ee235d681e9cbf966892bb27c2fb7
oajh04 <cloudtubekr@naver.com>
2021-06-28 23:14:06 +0900

    ⚡️ :: drag and drop 기능 추가

--- src/components/Project/Project.tsx
+++ src/components/Project/Project.tsx
@@ -1,6 +1,5 @@
 import { FC, useEffect, useState } from 'react';
-import { useRecoilState } from 'recoil';
-import organization from 'src/libs/api/organization';
+import { DragDropContext, Droppable, Draggable } from 'react-beautiful-dnd'
 import projects from 'src/libs/api/projects';
 import {RouteComponentProps} from 'react-router-dom'
 import Board from './Board/Board';
@@ -22,14 +21,45 @@ const Project: FC<RouteComponentProps<Props>> = ({match}) => {
         })
     },[]);
 
+    const handleChange = (result) => {
+        console.log("adws")
+        if (!result.destination) return;
+        console.log(result);
+        const items = [...columns];
+        const [reorderedItem] = items.splice(result.source.index, 1);
+        items.splice(result.destination.index, 0, reorderedItem);
+    
+        setColumns(items);
+
+        console.log(columns[result.destination.index-1])
+    };
+
     return(
         <S.Wrapper>
             <S.BoardTitle>{organizations?.name}</S.BoardTitle>
-            <S.BoardWrapper>
-                {
-                    columns.map((i,index)=><Board key={index} title={i.name} columns_id={i.id}></Board>)
-                }
-            </S.BoardWrapper>
+            
+                <DragDropContext >
+                    <Droppable droppableId="columns" onDragEnd={handleChange}>
+                        {(provided) => (
+                            <li className="columns" {...provided.droppableProps} ref={provided.innerRef}>
+                                <S.BoardWrapper>
+                                {
+                                    columns.map((i,index)=>(
+                                        <Draggable draggableId={String(i.id)} index={index}>
+                                            {(provided) => (
+                                                <li ref={provided.innerRef} {...provided.dragHandleProps} {...provided.draggableProps} >                                                
+                                                    <Board key={index} title={i.name} columns_id={i.id}></Board>
+                                                </li>
+                                            )}
+                                        </Draggable>
+                                    ))
+                                }
+                                </S.BoardWrapper>
+                                {provided.placeholder}
+                            </li>
+                        )}
+                    </Droppable>
+                </DragDropContext>
         </S.Wrapper>
     )
 }
