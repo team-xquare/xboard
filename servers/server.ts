c9f728ccda97261fe710ffc441b4788c4039dbb9
oajh04 <cloudtubekr@naver.com>
2021-07-07 11:16:02 +0900

    ⚡️ :: server route

--- servers/server.ts
+++ servers/server.ts
@@ -3,11 +3,12 @@ const app = express();
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const port =process.env.PORT || 3001;
+const route = require('./routes/index');
 
 //app.use(cors());
 
 app.use(bodyParser.json());
-app.use('/auth', (req, res)=> res.json({username:'bryan'}));
+app.use('/auth', route)
 
 app.listen(port, ()=>{
     console.log(`express is running on ${port}`);
