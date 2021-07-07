a1f49ed3c3570068d2197ed0978d45fdb78eea59
oajh04 <cloudtubekr@naver.com>
2021-07-07 11:09:04 +0900

    ⚡️ :: server proxy설정

---
+++ src/proxy/setupProxy.ts
@@ -0,0 +1,10 @@
+export const { createProxyMiddleware } = require('http-proxy-middleware');
+
+module.exports = function(app){
+  app.use(
+      createProxyMiddleware('/auth', {
+          target: 'http://localhost:3001/',
+          changeOrigin: true
+      })
+  )
+};
